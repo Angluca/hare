36039e8636c52be0cdc4304626570fd450b7602a
Pixeller <156404+Angluca@users.noreply.github.com>
2024-03-12 18:19:47 +0800

    Upgrade libc compiler

--- rt/+darwin/start+x86_64+libc.s
+++ rt/+darwin/start+x86_64+libc.s
@@ -7,20 +7,6 @@ _start:
 	call _rt.start_darwin
 
 
-.private_extern ___libc_init_array_start
-___libc_init_array_start = section$start$__DATA$.init_array
+.include "start+libc.s"
 
-.private_extern ___libc_init_array_end
-___libc_init_array_end = section$end$__DATA$.init_array
 
-.private_extern ___fini_array_start
-___fini_array_start = section$start$__DATA$.fini_array
-
-.private_extern ___fini_array_end
-___fini_array_end = section$end$__DATA$.fini_array
-
-.private_extern ___test_array_start
-___test_array_start = section$start$__DATA$.test_array
-
-.private_extern ___test_array_end
-___test_array_end = section$end$__DATA$.test_array
